9b250a87544bff1d5d8e798a2b6a4808ef529d8d
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-11-09 21:47:42 -0500

    Added the project stucture for the hardware interface

---
+++ include/talonSRX_hardware.h
@@ -0,0 +1,17 @@
+//
+// Created by ludovic on 09/11/19.
+//
+
+#include "hardware_interface/joint_state_interface.h"
+#include "hardware_interface/joint_command_interface.h"
+#include "hardware_interface/robot_hw.h"
+#include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
+
+#ifndef TAKIN_ROS_CONTROL_WS_TALONSRX_HARDWARE_H
+
+
+
+#define TAKIN_ROS_CONTROL_WS_TALONSRX_HARDWARE_H
+
+#endif //TAKIN_ROS_CONTROL_WS_TALONSRX_HARDWARE_H
---
+++ src/talonSRX_hardware_interface.cpp
@@ -0,0 +1,4 @@
+//
+// Created by ludovic on 09/11/19.
+//
+#include <talonSRX_hardware_interface.h>
