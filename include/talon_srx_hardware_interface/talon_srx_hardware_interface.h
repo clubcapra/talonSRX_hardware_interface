ffb2ae9d26a4a6426f7b9512bbd9c0b2bfd6e2cb
Ludovic Vanasse <ludovicvanasse@gmail.com>
2020-02-13 23:26:08 -0500

    Updated the formating of the files to follow the ROS clang-format

--- include/talon_srx_hardware_interface/talon_srx_hardware_interface.h
+++ include/talon_srx_hardware_interface/talon_srx_hardware_interface.h
@@ -5,49 +5,50 @@
 #define SRC_TALON_SRX_HARDWARE_INTERFACE_H
 // CTRE includes
 #include <dynamic_reconfigure/server.h>
-#include "ctre/Phoenix.h"
-#include "ctre/phoenix/unmanaged/Unmanaged.h"
 #include "Platform-linux-socket-can.h"
+#include "ctre/Phoenix.h"
 #include "ctre/phoenix/MotorControl/CAN/TalonSRX.h"
+#include "ctre/phoenix/unmanaged/Unmanaged.h"
 // ROS includes
-#include "geometry_msgs/Twist.h"
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
+#include "geometry_msgs/Twist.h"
 #include "hardware_interface/robot_hw.h"
-#include <controller_manager/controller_manager.h>
 #include "ros/ros.h"
 // STD includes
-#include <vector>
 #include <string>
-
+#include <vector>
 
 // Robot variable
-const int8_t MOTOR_COUNT = 4;// TODO Change it so it loads from a config file.
+const int8_t MOTOR_COUNT = 4;  // TODO Change it so it loads from a config file.
 
-namespace talon_srx_hardware_interface {
-    class talonSRXHardwareInterface : public hardware_interface::RobotHW {
-    public:
-        // Functions
-        bool init(ros::NodeHandle &robot_hw_nh);
+namespace talon_srx_hardware_interface
+{
+class talonSRXHardwareInterface : public hardware_interface::RobotHW
+{
+public:
+  // Functions
+  bool init(ros::NodeHandle &robot_hw_nh);
 
-        void read(const ros::Time &time, const ros::Duration &period);
+  void read(const ros::Time &time, const ros::Duration &period);
 
-        void write(const ros::Time &time, const ros::Duration &period);
+  void write(const ros::Time &time, const ros::Duration &period);
 
-        // Interface variables
-        std::vector<std::string> motors_name = {"drive_FL", "drive_FR", "drive_RL",
-                                                "drive_RR"}; // TODO Change it so it loads from a config file.
-        int FL = 0, FR = 0, RL = 0, RR = 0;
-        double cmd[MOTOR_COUNT];
-        double pos[MOTOR_COUNT];
-        double vel[MOTOR_COUNT];
-        double eff[MOTOR_COUNT];
-    private:
-        // Variables
-        hardware_interface::VelocityJointInterface joint_velocity_interface;
-        hardware_interface::JointStateInterface joint_state_interface;
-        std::vector<std::shared_ptr<TalonSRX>> left_track;
-        std::vector<std::shared_ptr<TalonSRX>> right_track;
-    };
-}
-#endif //SRC_TALON_SRX_HARDWARE_INTERFACE_H
+  // Interface variables
+  std::vector<std::string> motors_name = { "drive_FL", "drive_FR", "drive_RL",
+                                           "drive_RR" };  // TODO Change it so it loads from a config file.
+  int FL = 0, FR = 0, RL = 0, RR = 0;
+  double cmd[MOTOR_COUNT];
+  double pos[MOTOR_COUNT];
+  double vel[MOTOR_COUNT];
+  double eff[MOTOR_COUNT];
+
+private:
+  // Variables
+  hardware_interface::VelocityJointInterface joint_velocity_interface;
+  hardware_interface::JointStateInterface joint_state_interface;
+  std::vector<std::shared_ptr<TalonSRX>> left_track;
+  std::vector<std::shared_ptr<TalonSRX>> right_track;
+};
+}  // namespace talon_srx_hardware_interface
+#endif  // SRC_TALON_SRX_HARDWARE_INTERFACE_H
--- src/talon_srx_hardware_interface.cpp
+++ src/talon_srx_hardware_interface.cpp
@@ -4,46 +4,51 @@
 
 #include "talon_srx_hardware_interface/talon_srx_hardware_interface.h"
 
-namespace talon_srx_hardware_interface {
-    bool talonSRXHardwareInterface::init(ros::NodeHandle &hw_nh) {
-        // Get parameters (joint name, joint type, talonsrx id, etc)
-        // TODO Change so it can be more dynamic
-        // front
-        if (hw_nh.getParam("drives/drive_FL", this->FL))
-            this->left_track.push_back(std::make_shared<TalonSRX>(this->FL));
-        if (hw_nh.getParam("drives/drive_FR", this->FR))
-            this->right_track.push_back(std::make_shared<TalonSRX>(this->FR));
-        // rear
-        if (hw_nh.getParam("drives/drive_RL", this->RL))
-            this->left_track.push_back(std::make_shared<TalonSRX>(this->RL));
-        if (hw_nh.getParam("drives/drive_RR", this->RR))
-            this->right_track.push_back(std::make_shared<TalonSRX>(this->RR));
+namespace talon_srx_hardware_interface
+{
+bool talonSRXHardwareInterface::init(ros::NodeHandle &hw_nh)
+{
+  // Get parameters (joint name, joint type, talonsrx id, etc)
+  // TODO Change so it can be more dynamic
+  // front
+  if (hw_nh.getParam("drives/drive_FL", this->FL))
+    this->left_track.push_back(std::make_shared<TalonSRX>(this->FL));
+  if (hw_nh.getParam("drives/drive_FR", this->FR))
+    this->right_track.push_back(std::make_shared<TalonSRX>(this->FR));
+  // rear
+  if (hw_nh.getParam("drives/drive_RL", this->RL))
+    this->left_track.push_back(std::make_shared<TalonSRX>(this->RL));
+  if (hw_nh.getParam("drives/drive_RR", this->RR))
+    this->right_track.push_back(std::make_shared<TalonSRX>(this->RR));
 
-        // Intialize raw data
-        std::fill_n(this->pos, MOTOR_COUNT, 0.0);
-        std::fill_n(this->vel, MOTOR_COUNT, 0.0);
-        std::fill_n(this->eff, MOTOR_COUNT, 0.0);
-        std::fill_n(this->cmd, MOTOR_COUNT, 0.0);
+  // Intialize raw data
+  std::fill_n(this->pos, MOTOR_COUNT, 0.0);
+  std::fill_n(this->vel, MOTOR_COUNT, 0.0);
+  std::fill_n(this->eff, MOTOR_COUNT, 0.0);
+  std::fill_n(this->cmd, MOTOR_COUNT, 0.0);
 
-        // Initialize interface variables
-        for (int i = 0; i < MOTOR_COUNT; ++i) {
-            hardware_interface::JointStateHandle state_handle(this->motors_name[i], &pos[i], &vel[i], &eff[i]);
-            this->joint_state_interface.registerHandle(state_handle);
+  // Initialize interface variables
+  for (int i = 0; i < MOTOR_COUNT; ++i)
+  {
+    hardware_interface::JointStateHandle state_handle(this->motors_name[i], &pos[i], &vel[i], &eff[i]);
+    this->joint_state_interface.registerHandle(state_handle);
 
-            hardware_interface::JointHandle vel_handle(joint_state_interface.getHandle(this->motors_name[i]), &cmd[i]);
-            joint_velocity_interface.registerHandle(vel_handle);
-        }
-        // Register interfaces
-        registerInterface(&joint_state_interface);
-        registerInterface(&joint_velocity_interface);
-    }
-
-    void talonSRXHardwareInterface::read(const ros::Time &time, const ros::Duration &period) {
-        ROS_INFO("TEST READ");
-    }
-
-    void talonSRXHardwareInterface::write(const ros::Time &time, const ros::Duration &period) {
-        ROS_INFO("TEST WRITE");
-    }
-
+    hardware_interface::JointHandle vel_handle(joint_state_interface.getHandle(this->motors_name[i]), &cmd[i]);
+    joint_velocity_interface.registerHandle(vel_handle);
+  }
+  // Register interfaces
+  registerInterface(&joint_state_interface);
+  registerInterface(&joint_velocity_interface);
 }
+
+void talonSRXHardwareInterface::read(const ros::Time &time, const ros::Duration &period)
+{
+  ROS_INFO("TEST READ");
+}
+
+void talonSRXHardwareInterface::write(const ros::Time &time, const ros::Duration &period)
+{
+  ROS_INFO("TEST WRITE");
+}
+
+}  // namespace talon_srx_hardware_interface
